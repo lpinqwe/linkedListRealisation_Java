2b9de243a2ea1cb8b1694c85ed9ce03ef51be5f2
lpinqwe <gorelikovva@gmail.com>
2024-02-21 11:27:48 +0100

    add replace, insert, delete func in linked list it work.....

--- src/main/java/org/example/List_elem.java
+++ src/main/java/org/example/List_elem.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import java.util.Iterator;
-import java.util.List;
 import java.util.Objects;
 
 public class List_elem<A> implements Iterable<A> {
@@ -39,19 +38,59 @@ public class List_elem<A> implements Iterable<A> {
         return head == null;
     }
 
-
-    public void add_first(A item){
-        ListItem<A> newItem=new ListItem<A>();
-        newItem.data=item;
-        if(isEmpty()){
-            head=newItem;
-            tail=newItem;
-        }else{
+    //insert, delete, replace
+    public ListItem<A> delete(int index) {
+        ListItem<A> current = head;
+        index = index - 1;
+        if (index < 0) {
             ListItem<A> buf = head;
-            head=newItem;
-            head.next=buf;
+            head = head.next;
+            return buf;
+        }
+        //System.out.println("index=" + (index));
+        for (int i = 0; i != index; i++) {
+            current = current.next;
+        }
+        //System.out.println("CURRENT=" + current.data);
+        ListItem<A> buf = current.next;
+        current.next = current.next.next;
+        return buf;
+    }
+
+    public void insert(A item, int index) {
+        ListItem<A> newItem=new ListItem<>();
+        ListItem<A> current= head;
+        newItem.data=item;
+        index=index-1;
+        if(index<0){
+            add_first(item);
+            return;
+        }
+        for(int i =0;i!=index;i++){
+            current=current.next;
+        }
+        ListItem<A>buf=current.next;
+        current.next=newItem;
+        newItem.next=buf;
+    }
+    public ListItem<A> replace(A item,int index){
+        insert(item,index);
+        return delete(index+1);
+
+    }
+    public void add_first(A item) {
+        ListItem<A> newItem = new ListItem<A>();
+        newItem.data = item;
+        if (isEmpty()) {
+            head = newItem;
+            tail = newItem;
+        } else {
+            ListItem<A> buf = head;
+            head = newItem;
+            head.next = buf;
         }
     }
+
     public void addEnd(A item) {
         ListItem<A> newItem = new ListItem<>();
         newItem.data = item;
@@ -66,14 +105,14 @@ public class List_elem<A> implements Iterable<A> {
 
     public void reverse() {
         if (!isEmpty() && head.next != null) {
-            tail=head;
+            tail = head;
             ListItem<A> current = head.next;
-            head.next=null;
-            while (current!=null){
+            head.next = null;
+            while (current != null) {
                 ListItem<A> next = current.next;
-                current.next= head;
-                head=current;
-                current=next;
+                current.next = head;
+                head = current;
+                current = next;
 
             }
         }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,16 +3,22 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         List_elem<Example> list = new List_elem<>();
+        //list.add_first(new Example(888, "888", "1111111"));
+        //list.add_first(new Example(999, "999", "0000000"));
+        list.addEnd(new Example(0, "a", "+2112"));
         list.addEnd(new Example(1, "a", "+2112"));
-        list.add_first(new Example(888, "888", "1111111"));
-
         list.addEnd(new Example(2, "b", "+2112"));
         list.addEnd(new Example(3, "c", "+2112"));
-        list.add_first(new Example(999, "999", "0000000"));
         list.addEnd(new Example(4, "d", "+2112"));
         list.addEnd(new Example(5, "e", "+2112"));
-
         list.reverse();
+
+        //System.out.println("DELETED " + list.delete(1).data);
+        Example elem = new Example(41, "d", "+2112");
+        list.replace(elem,1);
+
+
+
         for (Example exampleaa : list) {
             System.out.println(exampleaa);
         }
